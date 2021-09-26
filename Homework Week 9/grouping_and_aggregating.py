e193e1fe8eab0e4bcc1c532428a2841dbaec563f
Daniel Goh U-Sheng <dgoh0005@student.monash.edu>
2021-09-26 14:56:10 +0800

    push changes

---
+++ Homework Week 9/grouping_and_aggregating.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+employed = pd.read_csv("C:/Users/user/Desktop/FIT3179/FIT3179/Visualisation 2 Datasets/Employed person by sex and state 1982 to 2020 Malay.csv")
+
+#Group by two keys and then summarize each group
+employed_population =  employed.groupby(['State/Country'],as_index=False).agg({'Employed (\'000)':['sum']})
+print(employed_population)
+
+employed_population.to_csv(r"C:\Users\user\Desktop\FIT3179\FIT3179\Homework Week 9\Week 9 Homework Choropleth Map\cleaned Employed person by sex and state 1982 to 2020 Malay.csv", index = False)
