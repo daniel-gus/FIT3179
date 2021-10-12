096a7a99ccd9f5f23d77a8fdf6043bb3fff1b261
Daniel Goh U-Sheng <dgoh0005@student.monash.edu>
2021-10-12 15:29:26 +0800

    push file renaming

--- Visualisation_2/Datasets/data_cleaner.py
+++ Visualisation_2/Datasets/data_cleaner.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
+employed = pd.read_csv("C:/Users/user/Desktop/FIT3179/FIT3179/Visualisation 2/Datasets/Employed person by sex and state 1982 to 2020 Malay.csv")
 
+print(employed)
+
+'''
 employed = pd.read_csv("C:/Users/user/Desktop/FIT3179/FIT3179/Visualisation 2 Datasets/Employed person by sex and state 1982 to 2020 Malay.csv")
 
 #Group by two keys and then summarize each group
@@ -8,3 +12,4 @@ employed_population =  employed.groupby(['State/Country'],as_index=False).agg(su
 print(employed_population)
 
 employed_population.to_csv(r"C:\Users\user\Desktop\FIT3179\FIT3179\Homework Week 9\Week 9 Homework Choropleth Map\cleaned Employed person by sex and state 1982 to 2020 Malay.csv", index = False)
+'''
