67f2e49fd562f99613052b03963915b2594934a8
Daniel Goh U-Sheng <dgoh0005@student.monash.edu>
2021-10-12 16:06:13 +0800

    push changes

--- Visualisation_2/Datasets/data_cleaner.py
+++ Visualisation_2/Datasets/data_cleaner.py
@@ -1,15 +1,11 @@
 import pandas as pd
 
-employed = pd.read_csv("C:/Users/user/Desktop/FIT3179/FIT3179/Visualisation 2/Datasets/Employed person by sex and state 1982 to 2020 Malay.csv")
+employed = pd.read_csv("C:/Users/user/Desktop/FIT3179/FIT3179/Visualisation_2/Datasets/Employed_person_by_sex_and_state_1982_to_2020_Malaysia.csv")
 
+employed.loc[(employed["state"] == "Pulau Pinang"), "state"] = "Penang"
 print(employed)
 
-'''
-employed = pd.read_csv("C:/Users/user/Desktop/FIT3179/FIT3179/Visualisation 2 Datasets/Employed person by sex and state 1982 to 2020 Malay.csv")
+x = employed[employed['state']=='Penang']
+print(x)
 
-#Group by two keys and then summarize each group
-employed_population =  employed.groupby(['State/Country'],as_index=False).agg(sum_employed=('Employed (\'000)', 'sum'))
-print(employed_population)
-
-employed_population.to_csv(r"C:\Users\user\Desktop\FIT3179\FIT3179\Homework Week 9\Week 9 Homework Choropleth Map\cleaned Employed person by sex and state 1982 to 2020 Malay.csv", index = False)
-'''
+employed.to_csv(r"C:\Users\user\Desktop\FIT3179\FIT3179\Visualisation_2\Datasets\Employed_person_by_sex_and_state_1982_to_2020_Malaysia.csv", index = False)
